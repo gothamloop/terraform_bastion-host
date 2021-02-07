fd63c4cef25edcf49ad949c6d5bf926c4c877c88
gothamloop <g.otham@webtunnelo.com>
2021-02-07 20:10:53 +0100

    Terraform on AWS

--- vpc.tf
+++ vpc.tf
@@ -231,7 +231,7 @@ resource "aws_security_group" "bastion-sg" {
 
 resource "aws_key_pair" "bastion_key" {
    key_name   = "my-key-pair"
-   public_key = "ssh-rsa xxxx"
+   public_key = file("${path.module}/id_rsa.pub")
 }
 
 output "bastion_public_ip" {
